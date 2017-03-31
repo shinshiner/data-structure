e8ce5539805612d8fef64d6b49b45aa348b407ab
ZeLin.Ye_SJTU_MVIG_training <qiongwuhero@163.com>
2017-03-31 12:15:03 +0800

    树状数组

--- Binary-Indexed-Tree/BIT.hpp
+++ Binary-Indexed-Tree/BIT.hpp
@@ -1,5 +1,5 @@
 const int MAX = 100000;
-int T[MAX]={0};     //数组初始值必须为0
+int T[MAX]={0};     //数组的初始值必须为0
 void Add(int x, int p){   //在x处增加p的值，数组的构造必须用此函数，不能直接赋值
     for(int i=x;i<=MAX;i+=(i&-i)) T[i] += p;
 }
